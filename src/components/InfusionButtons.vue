1385317374eeae929906afe9086d00ac27807f90
thomasvanrenslumiad <thomasvanrens@lumiad.com>
2025-04-07 11:47:41 +0200

    new font added, added scaling for live ipade test added network dev enviroment for testing

--- src/components/InfusionButtons.vue
+++ src/components/InfusionButtons.vue
@@ -41,19 +41,19 @@ const backgroundClass = computed(() => {
 const notifier = computed(() => {
   if (props.id === selectedButtoneStore.pressedButtonId) {
     if (props.remainingMl === 0) {
-      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[92vw] w-[86vw] cursor-pointer rounded-full bg-red-700 hover:bg-red-500 text-white outline-2 outline-offset-2 outline-red-800 outline-5 '
+      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[29.5VW] w-[86vw] cursor-pointer rounded-full bg-red-700 hover:bg-red-500 text-white outline-2 outline-offset-2 outline-red-800 outline-5 '
     } else if (props.mlPerHour === 0) {
-      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[92vw] w-[86vw] cursor-pointer rounded-full bg-yellow-700 hover:bg-yellow-500 text-white outline-2 outline-offset-2 outline-red-500 outline-3'
+      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[29.5VW] w-[86vw] cursor-pointer rounded-full bg-yellow-700 hover:bg-yellow-500 text-white outline-2 outline-offset-2 outline-red-500 outline-3'
     } else {
-      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[92vw] w-[86vw] cursor-pointer rounded-full bg-gray-700 hover:bg-gray-500 outline-2 text-white outline-offset-2 outline-black active:text-white dark:bg-gray-300 dark:hover:bg-gray-400'
+      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[29.5VW] w-[86vw] cursor-pointer rounded-full bg-gray-700 hover:bg-gray-500 outline-2 text-white outline-offset-2 outline-black active:text-white dark:bg-gray-300 dark:hover:bg-gray-400'
     }
   } else {
     if (props.remainingMl === 0) {
-      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[92vw] w-[86vw] cursor-pointer rounded-full bg-red-200 hover:bg-red-500 hover:text-white focus:bg-red-700 focus:text-white focus:outline-2 data-[state=on]:bg-red-700 focus:outline-offset-2 focus:outline-red-800 active:text-white outline-red-500 outline-5'
+      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[29.5VW] w-[86vw] cursor-pointer rounded-full bg-red-200 hover:bg-red-500 hover:text-white focus:bg-red-700 focus:text-white focus:outline-2 data-[state=on]:bg-red-700 focus:outline-offset-2 focus:outline-red-800 active:text-white outline-red-500 outline-5'
     } else if (props.mlPerHour === 0) {
-      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[92vw] w-[86vw] cursor-pointer rounded-full bg-yellow-200 hover:bg-yellow-500 hover:text-white focus:bg-yellow-700 data-[state=on]:bg-yellow-700 focus:text-white focus:outline-2 focus:outline-offset-2 focus:outline-red-500 active:text-white outline-orange-500 outline-3  '
+      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[29.5VW] w-[86vw] cursor-pointer rounded-full bg-yellow-200 hover:bg-yellow-500 hover:text-white focus:bg-yellow-700 data-[state=on]:bg-yellow-700 focus:text-white focus:outline-2 focus:outline-offset-2 focus:outline-red-500 active:text-white outline-orange-500 outline-3  '
     } else {
-      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[92vw] w-[86vw] cursor-pointer rounded-full bg-gray-400 hover:bg-gray-500 hover:text-white focus:bg-gray-700 data-[state=on]:bg-gray-700 focus:text-white focus:outline-2 focus:outline-offset-2 focus:outline-black active:text-white dark:bg-gray-300 dark:hover:bg-gray-400  '
+      return 'm-2 flex 4xl:w-[27VW] xl:w-[25.5VW] md:w-[29.5VW] w-[86vw] cursor-pointer rounded-full bg-gray-400 hover:bg-gray-500 hover:text-white focus:bg-gray-700 data-[state=on]:bg-gray-700 focus:text-white focus:outline-2 focus:outline-offset-2 focus:outline-black active:text-white dark:bg-gray-300 dark:hover:bg-gray-400  '
     }
   }
 })
